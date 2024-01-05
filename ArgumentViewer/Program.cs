3c937831f586c40d4b2e656a2012391ac814fbf2
Halit İNCE <halitince80@gmail.com>
2024-01-05 16:25:33 +0300

    Add project files.

---
+++ ArgumentViewer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace ArgumentViewer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argument Viewer");
+            Console.WriteLine("");
+            Console.WriteLine("Lenght: " + args.Length);
+            Console.WriteLine("");
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"argument[{i}]: " + args[i]);
+                var decodeValue = WebUtility.UrlDecode(args[i]);
+                Console.WriteLine($"[UrlDecode]: " + decodeValue);
+                Console.WriteLine("");
+            }
+
+            while (Console.ReadLine() != "Exist")
+            { }
+        }
+    }
+}
